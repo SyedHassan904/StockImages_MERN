b3cfc27049acac830ac94717f909e87ff9985621
Syed Hassan <syedhassanshah154@gmail.com>
2025-07-19 20:54:30 +0500

    Update index.js

--- stockBackend/index.js
+++ stockBackend/index.js
@@ -21,11 +21,14 @@ Cloudinary();
 
 const app = express();
 
-const allowedOrigin = process.env.FRONTEND_URL;
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.ADMIN_URL
+];
 
 app.use(cors({
-  origin: allowedOrigin,
-  credentials: true
+  origin: allowedOrigins,
+  credentials: true // if you're using cookies or auth headers
 }));
 
 
