8f17e3e68c0dc434b3062c9f202a7ede97dd18d2
Syed Hassan <syedhassanshah154@gmail.com>
2025-07-22 21:56:10 +0500

    Update authRoutes.js

--- stockBackend/routes/authRoutes.js
+++ stockBackend/routes/authRoutes.js
@@ -5,7 +5,7 @@ import Auth from '../controllers/Auth.js';
 import isAuth from '../middlewares/isAuth.js';
 import isAdmin from '../middlewares/isAdmin.js'
 
-const { RegisterUser, LoginUser, adminLogin, verifyAdmin, updateUser } = Auth
+const { RegisterUser, LoginUser, adminLogin, verifyAdmin, updateUser, forgotPassword, resetPassword } = Auth
 const authRouter = express.Router();
 
 authRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
@@ -22,6 +22,8 @@ authRouter.post("/login", LoginUser)
 authRouter.post("/admin/login", adminLogin)
 authRouter.get("/admin/verify", isAdmin, verifyAdmin)
 authRouter.put("/updateuser", isAuth, updateUser)
+authRouter.post("/forgotpassword", forgotPassword)
+authRouter.post("/reset-password", resetPassword)
 
 
 export default authRouter;
