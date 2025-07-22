07026d5bc047671431e22b6b5afe471b0d400472
Syed Hassan <syedhassanshah154@gmail.com>
2025-07-22 16:50:17 +0500

    Update passport.js

--- stockBackend/config/passport.js
+++ stockBackend/config/passport.js
@@ -24,7 +24,7 @@ passport.use(
   new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    callbackURL: "/auth/google/callback",
     passReqToCallback: true
   },
     async (req, accessToken, refreshToken, profile, done) => {
